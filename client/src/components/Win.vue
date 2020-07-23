14b933d0d94b2071c6caa1d6c108404715387d80
Galih.H <zenilevh@gmail.com>
2020-07-23 22:37:59 +0700

    +add win.vue

---
+++ client/src/components/Win.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="container border rounded-lg shadow p-3 mb-5 bg-white rounded"
+    style="width: 700px"
+    id="WIN"
+  >
+    <h1 class="text-center display-5">CONGRATS BRUH LU MENANG</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+
+<style>
+</style>
