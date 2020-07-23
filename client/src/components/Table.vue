e829e9af2d3a3e1d94ed9133df22f72143fd1a50
Galih.H <zenilevh@gmail.com>
2020-07-23 21:09:09 +0700

    initial commit client folder

---
+++ client/src/components/Table.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container border rounded-lg shadow p-3 mb-5 bg-white rounded">
+    <button type="button" class="btn btn-success mt-2">Create Room</button>
+    <table class="table table-striped table-dark mt-3 text-center">
+      <thead>
+        <tr>
+          <th scope="col">Room Name</th>
+          <th scope="col">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">Larry</th>
+          <td>
+            <button class="btn btn-success">Join</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <Table></Table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Table from "../components/Table.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Table,
+  },
+};
+</script>
+
+
+<style >
+#quick {
+  font-family: "Bungee Shade", cursive;
+  color: rgb(105, 80, 80);
+}
+</style>
