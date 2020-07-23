faf51ca8a9a7fc05ba8ec3679c1333bdcf27fc89
Adek Ferdian <adekferdian@gmail.com>
2020-07-23 23:01:58 +0700

    + update index.js :  - > added login routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,12 +4,18 @@ import Home from '../views/Home.vue';
 import Waiting from '../components/Waiting.vue';
 import Win from '../components/Win.vue';
 import Game from '../views/Game.vue';
-
+import Login from '../views/Login.vue';
 
 
 Vue.use(VueRouter);
 
-const routes = [{
+const routes = [
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
+    },
+    {
         path: '/',
         name: 'Home',
         component: Home,
